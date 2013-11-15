c23db1b98e715adb707c5fc0a10699f702412ac4
Travis Vachon <travis@copious.com>
2013-11-15 14:18:29 -0500

    add url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject broken-template/lein-template "0.1.0-SNAPSHOT"
   :description "This is a broken lein template. It is used for tests."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/travis/broken-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
